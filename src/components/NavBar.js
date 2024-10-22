b45e2627faf3d21da8a48f6d4ef6e6131149a7f1
Spanos <nickspanosf1@gmail.com>
2024-10-22 21:12:01 +0300

    Add the NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../NavBar.css';
+
+export function NavBar() {
+    return (
+        <nav>
+            <h1><span>Quote Of</span> The Day</h1>
+            <button>Favourites</button>
+        </nav>
+
+    )
+}
